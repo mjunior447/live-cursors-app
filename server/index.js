8623f2fa1410b1c725a9d70b330809adb1276312
Mauricio Antero <mjunior447@gmail.com>
2025-07-18 19:50:55 -0300

    feat: create server initial structure

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const { WebSocketServer } = require("ws");
+
+const server = http.createServer();
+const wsServer = new WebSocketServer({
+  server,
+});
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`ws server is running on port ${port}`);
+});
